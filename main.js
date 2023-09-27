230260718bd0dcc33b76f3f9f63e3c7d16f5aa2a
licecarioca <145568850+licecarioca@users.noreply.github.com>
2023-09-27 11:07:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.querySelector('.teclado');
+function tocaSomPom(){
+  document.querySelector('toca_som_pom').play('');
+} 
+
+document.querySelectorAll('.tecla')
+
+//document.querySelector(.tecla_pom).onclick = toca 
+document.querySelector('.tecla_pom').onclick = tocaSomPom ();
