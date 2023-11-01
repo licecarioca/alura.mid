d5d654120527be02939eed96682bfa301932a337
licecarioca <145568850+licecarioca@users.noreply.github.com>
2023-11-01 10:41:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-function tocaSomPom(idElementoAudio){
-    document.querSelector(idElementoAudio)
+function tocaSomPom(seletorAudio){
+    const elemento = document.querSelector(seletorAudio).play('');
+
+    if(elemento != null && elemento .localName === 'audio'){
+        elemento.play();
+    }else{
+        alert('Elemento não encontrado')
+
 }
 
-document.querySelectorAll('.tecla')
+document.querySelectorAll('.tecla');
 
 
 
 for(contador = 0; < listadeTeclas.length; contador++){
    
 
-
-
- const instrumento = listadeTeclas[contador] 
+    const instrumento = listadeTeclas[contador];
   
   const tecla = listadeTeclas[contador];
 
   const ideAudio = `som_${instrumento}`;
 
-  tecla.onclick = fuction()
+  tecla.onclick = fuction(){
         tocaSom(ideAudio);
-};
+  };
+        tecla.onkeydown = function (evento){
+            if(evento.code === 'Space'||evento.code === 'Enter' ){
+            tecla.classList.add('ativa');
+        }
+    }
+tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
+}
+}
